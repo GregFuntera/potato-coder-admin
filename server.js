6d06c8ce3543bad4e98a2d93f845aeb645a52f3c
Greg Funtera <greg.funtera@gmail.com>
2018-05-12 15:47:27 +0800

    - Fixed issue regarding deployment

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/potato-coder-admin'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname+ '/dist/potato-coder-admin/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-console.log(process.env.PORT);
+console.log(process.env.PORT || 8080);
